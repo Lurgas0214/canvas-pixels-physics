9212742d7dfab6e8edf7d5e38cbd9fe1059ca142
Lurgas <dbunk0214@gmail.com>
2022-12-13 11:55:59 +0100

    stateless

--- src/components/CanvasComponent.tsx
+++ src/components/CanvasComponent.tsx
@@ -5,6 +5,10 @@ const getAlpha = (pixels: Uint8ClampedArray, index: number): number => {
     return pixels[index + 3];
 };
 
+const getRndNumber = (range: number): number => {
+    return range - (Math.random() * range * 2)
+}
+
 const getColor = (pixels: Uint8ClampedArray, index: number): string => {
     const red = pixels[index], green = pixels[index + 1], blue = pixels[index + 2];
     return ('rgb(' + red + ',' + green + ',' + blue + ')');
@@ -21,10 +25,10 @@ class Mouse {
 }
 
 class Particle {
-    ease: number;
-    color: string;
     originX: number;
     originY: number;
+    color: string;
+    ease: number;
     size: number;
     vx: number;
     vy: number;
@@ -32,16 +36,16 @@ class Particle {
     y: number;
     v: number;
 
-    constructor(color: string, size: number, ease: number, x: number, y: number, v: number) {
-        this.vx = v - (Math.random() * v * 2);
-        this.vy = v - (Math.random() * v * 2);
+    constructor(radius: number, color: string, size: number, ease: number, x: number, y: number, v: number) {
+        this.x = Math.floor(getRndNumber(radius));
+        this.y = Math.floor(getRndNumber(radius));
+        this.vx = Math.floor(getRndNumber(v));
+        this.vy = Math.floor(getRndNumber(v));
         this.originX = Math.floor(x);
         this.originY = Math.floor(y);
         this.size = Math.floor(size);
         this.color = color;
         this.ease = ease;
-        this.x = x;
-        this.y = y;
         this.v = v;
     }
 
@@ -52,14 +56,14 @@ class Particle {
         }
     }
 
-    reverse() {
+    return2Origin() {
         this.x += (this.originX - this.x) * this.ease;
         this.y += (this.originY - this.y) * this.ease;
     }
 
-    update() {
-        this.x += this.vx;
-        this.y += this.vy;
+    update(x: number, y: number) {
+        this.x = x;
+        this.y = y;
     }
 }
 
@@ -75,101 +79,54 @@ class CanvasComponent extends React.Component<CanvasProps, CanvasStateProps> {
     canvasContext: CanvasRenderingContext2D | null = null;
     canvasElement: HTMLCanvasElement | null = null;
     imageElement: HTMLImageElement | null = null;
-    reAssembleID: number | null = null;
-    explosionID: number | null = null;
+    particles: Particle[] = [];
     mouse: Mouse = new Mouse();
+    radius: number = 3000;
+    velocity: number = 33;
+    ease: number = 0.2;
+    gap: number = 5;
 
-    constructor(props: CanvasProps) {
-        super(props);
-        this.state = {};
-    }
-
-    componentDidMount(): void {
-        this.initialize();
-    }
-
-    componentDidUpdate(): void {
-        this.clearCanvas();
-        this.drawParticles();
-    }
-
-    initialize = () => {
-        const { canvasElement, canvasContext, imageElement } = this, particles: Particle[] = [], ease: number = 0.2, gap: number = 5, v: number = 33;
-        let pixels: Uint8ClampedArray | null = null, color: string, alpha: number, index: number, x: number, y: number;
-
+    componentDidMount() {
+        const { canvasElement, canvasContext, imageElement, particles, velocity, radius, ease, gap } = this;
         if (canvasElement && canvasContext && imageElement) {
             imageElement.onload = () => {
-                const shift = 0.2, stretch = 1 - (shift * 2),
-                    dx = canvasElement.width * shift, dy = canvasElement.height * shift,
-                    dw = canvasElement.width * stretch, dh = canvasElement.height * stretch;
+                const shift = 0.2, stretch = 1 - (shift * 2), dx = canvasElement.width * shift, dy = canvasElement.height * shift, dw = canvasElement.width * stretch, dh = canvasElement.height * stretch;
+                let pixels: Uint8ClampedArray | null = null, color: string, alpha: number, index: number, x: number, y: number;
                 canvasContext.drawImage(imageElement, dx, dy, dw, dh);
-
                 pixels = canvasContext.getImageData(0, 0, canvasElement.width, canvasElement.height).data;
-
                 for (x = 0; x < canvasElement.width; x += gap) {
                     for (y = 0; y < canvasElement.height; y += gap) {
                         index = (y * canvasElement.width + x) * 4;
                         color = getColor(pixels, index);
                         alpha = getAlpha(pixels, index);
                         if (alpha > 0) {
-                            particles.push(new Particle(color, ease, gap, x, y, v));
+                            particles.push(new Particle(radius, color, gap, ease, x, y, velocity));
                         }
                     }
                 }
-
-                this.setState({ particles: particles });
             };
         }
-    }
-
-    clearCanvas = () => {
-        const { canvasElement, canvasContext } = this;
-        if (canvasContext && canvasElement) {
-            canvasContext.clearRect(0, 0, canvasElement.width, canvasElement.height);
-        }
-    }
-
-    drawParticles = () => {
-        const { particles } = this.state;
-        if (this.canvasContext && particles) {
-            particles.forEach(particle => { particle.draw(this.canvasContext); });
-        }
-    }
-
-    explodeImage = () => {
-        const { particles } = this.state;
-        if (this.reAssembleID) {
-            cancelAnimationFrame(this.reAssembleID);
-            this.reAssembleID = null;
-        }
-        if (particles) {
-            particles.forEach(particle => { particle.update(); });
-            this.setState({ particles });
-        }
-        this.explosionID = requestAnimationFrame(this.explodeImage);
-    }
-
-    reAssembleImage = () => {
-        const { particles } = this.state;
-        if (this.explosionID) {
-            cancelAnimationFrame(this.explosionID);
-            this.explosionID = null;
-        }
-        if (particles) {
-            particles.forEach(particle => { particle.reverse() });
-            this.setState({ particles });
-        }
-        this.reAssembleID = requestAnimationFrame(this.reAssembleImage);
-    }
+        this.warp();
+    };
 
     warp = () => {
-        if (this.explosionID) this.reAssembleImage();
-        else this.explodeImage();
-    }
+        const { canvasElement, canvasContext, particles } = this;
+        if (canvasContext && canvasElement) {
+            canvasContext.clearRect(0, 0, canvasElement.width, canvasElement.height);
+            particles.forEach(particle => particle.return2Origin());
+            particles.forEach(particle => particle.draw(this.canvasContext));
+            requestAnimationFrame(this.warp);
+        }
+    };
 
     mouseEvent = (event: MouseEvent) => {
         this.mouse.update(event.x, event.y);
-    }
+    };
+
+    mouseClick = () => {
+        const { particles, radius } = this;
+        particles.forEach(particle => particle.update(Math.floor(getRndNumber(radius)), Math.floor(getRndNumber(radius))));
+    };
 
     render() {
         const { base64String } = this.props;
@@ -183,7 +140,7 @@ class CanvasComponent extends React.Component<CanvasProps, CanvasStateProps> {
                             htmlElement.height = htmlElement.parentElement.clientHeight;
                         }
                         this.canvasElement = htmlElement;
-                        this.canvasElement.onclick = this.warp;
+                        this.canvasElement.onclick = this.mouseClick;
                         this.canvasElement.onmousemove = this.mouseEvent;
                         this.canvasContext = htmlElement.getContext('2d');
                     }
